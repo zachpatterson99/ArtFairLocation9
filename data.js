07092f51f0f247c4d8d6fc8db7b42016ab01204a
zachpatterson99 <zachpatterson99@gmail.com>
2024-07-05 16:30:09 -0400

    commit

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-location-9-thompson--william",
+      "name": "Location #9 Thompson & William",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.09148156291641563,
+        "pitch": 0.15535827943498148,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.5889599373109178,
+          "pitch": -0.07448389678534184,
+          "title": "Emergency Location",
+          "text": "<h1 class=\"DUwDvf lfPIob\" style=\"background: rgb(255, 255, 255); border: 0px; border-radius: 0px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; font-size: 1.375rem; line-height: 1.75rem; font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; list-style: none; outline: 0px; overflow: visible; vertical-align: baseline; color: rgb(32, 33, 36);\">Tower Plaza</h1>"
+        }
+      ]
+    }
+  ],
+  "name": "Location #9 - Thompson & William",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
